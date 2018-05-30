e922fa169c78e911620cc2c65263feeddc88a90c
Colin Russell <colinrussell101@gmail.com>
2018-05-30 17:12:30 -0400

    added temp test camera overlay to PlayerSetupViewController

--- DigitalTag/PlayerSetupViewController.swift
+++ DigitalTag/PlayerSetupViewController.swift
@@ -40,6 +40,12 @@ class PlayerSetupViewController: UIViewController, UIImagePickerControllerDelega
         imagePicker.delegate = self
         imagePicker.cameraDevice = .front
         
+        // Create the Camera Overlay
+        let cameraOverlay = UIView(frame: CGRect(x: self.view.frame.midY, y: self.view.frame.midY, width: self.view.frame.width/2, height: self.view.frame.height/2))
+        cameraOverlay.backgroundColor = UIColor.blue
+        cameraOverlay.alpha = 0.5
+        imagePicker.cameraOverlayView = cameraOverlay
+        
         present(imagePicker, animated: true)
     }
     
