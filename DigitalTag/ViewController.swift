b87c056e6c990e7c8baa3ee4fbad71995e573386
irawrmike <irmike@gmail.com>
2018-05-30 15:24:39 -0400

    Initial Commit

---
+++ DigitalTag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigitalTag
+//
+//  Created by Mike Stoltman on 2018-05-30.
+//  Copyright © 2018 Mike Stoltman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
