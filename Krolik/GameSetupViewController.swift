e7fd2f1819d8fc2244349df25f3edb3e95d4ef6a
Colin Russell <colinrussell101@gmail.com>
2018-05-31 16:22:55 -0400

    started to add collection view to a new GameStatusViewController

---
+++ Krolik/GameSetupViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GameSetupViewController.swift
+//  Krolik
+//
+//  Created by Colin on 2018-05-31.
+//  Copyright © 2018 Mike Stoltman. All rights reserved.
+//
+
+import UIKit
+
+class GameSetupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ Krolik/PlayerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerCollectionViewCell.swift
+//  Krolik
+//
+//  Created by Colin on 2018-05-31.
+//  Copyright © 2018 Mike Stoltman. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCollectionViewCell: UICollectionViewCell {
+    
+}
