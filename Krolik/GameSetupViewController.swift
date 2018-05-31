284e3c50f75743d32c908248fa10a440e34c6328
Colin Russell <colinrussell101@gmail.com>
2018-05-31 17:53:16 -0400

    added CollectionView with temp photos from url

--- Krolik/GameSetupViewController.swift
+++ Krolik/GameSetupViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class GameSetupViewController: UIViewController {
-
+class GameSetupViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "playerCell", for: indexPath)
+        
+        let imageView = UIImageView(frame: cell.contentView.frame)
+        imageView.contentMode = .scaleAspectFit
+        
+        getDataFromUrl(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/krolik-9ed87.appspot.com/o/GameID%2Fplayer01.png?alt=media&token=376d3c90-8c71-42a7-a673-612deafb8b4b")!) { (data, response, error) in
+            guard let image = UIImage(data: data!) else {
+                print("DATA ERROR from the url")
+                return
+            }
+            DispatchQueue.main.async {
+                imageView.image = image
+                self.collectionView.cellForItem(at: indexPath)?.contentView.addSubview(imageView)
+            }
+        }
+        return cell
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            completion(data, response, error)
+            }.resume()
+    }
 }
