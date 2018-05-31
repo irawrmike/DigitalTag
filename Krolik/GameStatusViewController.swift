2517e0536eb7771c049b0d5a3243a065218fa0fe
Colin Russell <colinrussell101@gmail.com>
2018-05-31 18:58:18 -0400

    added some padding to the cells in the GameStatusViewController content view

--- Krolik/GameStatusViewController.swift
+++ Krolik/GameStatusViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FirebaseStorage
 
-class GameSetupViewController: UIViewController, UICollectionViewDataSource {
+class GameStatusViewController: UIViewController, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -30,15 +30,16 @@ class GameSetupViewController: UIViewController, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "playerCell", for: indexPath)
-        //cell.backgroundColor = UIColor.gray
+        cell.contentView.layer.borderWidth = 2
+        cell.contentView.layer.cornerRadius = 10
+        cell.contentView.layer.borderColor = UIColor.black.cgColor
+        cell.layer.masksToBounds = true
         
-        let imageView = UIImageView(frame: cell.contentView.frame)
+        let cellFrame = cell.contentView.frame
+        let imageFrame = CGRect(x: cellFrame.origin.x+10, y: cellFrame.origin.y+10, width: cellFrame.width-20, height: cellFrame.height-20)
+        let imageView = UIImageView(frame: imageFrame)
         imageView.contentMode = .scaleAspectFit
-        imageView.layer.borderWidth = 2
-        imageView.layer.cornerRadius = 10
-        imageView.layer.masksToBounds = true
-        imageView.layer.borderColor = UIColor.black.cgColor
-        
+ 
         getDataFromUrl(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/krolik-9ed87.appspot.com/o/GameID%2Fplayer01.png?alt=media&token=376d3c90-8c71-42a7-a673-612deafb8b4b")!) { (data, response, error) in
             guard let image = UIImage(data: data!) else {
                 print("DATA ERROR from the url")
