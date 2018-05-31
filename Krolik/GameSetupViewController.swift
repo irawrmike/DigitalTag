a1a1c176791d0605ea8f68c0160c2c864dbb4a52
Colin Russell <colinrussell101@gmail.com>
2018-05-31 18:22:30 -0400

    added a border to the cells in the collection view

--- Krolik/GameSetupViewController.swift
+++ Krolik/GameSetupViewController.swift
@@ -30,9 +30,14 @@ class GameSetupViewController: UIViewController, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "playerCell", for: indexPath)
+        //cell.backgroundColor = UIColor.gray
         
         let imageView = UIImageView(frame: cell.contentView.frame)
         imageView.contentMode = .scaleAspectFit
+        imageView.layer.borderWidth = 2
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderColor = UIColor.black.cgColor
         
         getDataFromUrl(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/krolik-9ed87.appspot.com/o/GameID%2Fplayer01.png?alt=media&token=376d3c90-8c71-42a7-a673-612deafb8b4b")!) { (data, response, error) in
             guard let image = UIImage(data: data!) else {
