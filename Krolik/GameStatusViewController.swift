c8aca5473aaa7077f77fa1b41440837c3209dc59
Colin Russell <colinrussell101@gmail.com>
2018-06-13 16:50:11 -0400

    moved refresh currentPlayer to viewDidAppear in GameStatusViewController

--- Krolik/GameStatusViewController.swift
+++ Krolik/GameStatusViewController.swift
@@ -66,7 +66,17 @@ class GameStatusViewController: UIViewController, UICollectionViewDataSource {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+//    override func viewWillAppear(_ animated: Bool) {
+//        database.read(playerID: UserDefaults.standard.string(forKey: Player.keys.id)!) { (player) in
+//            guard let current = player else { return }
+//
+//            DispatchQueue.main.async {
+//                self.currentPlayer = current
+//                self.collectionView.reloadData()
+//            }
+//        }
+//    }
+    override func viewDidAppear(_ animated: Bool) {
         database.read(playerID: UserDefaults.standard.string(forKey: Player.keys.id)!) { (player) in
             guard let current = player else { return }
             
